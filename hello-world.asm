2d20e31461e50e403a74aa55cba4490d3f3f1f3a
zanex <81015632+zanexGHG@users.noreply.github.com>
2023-05-31 01:13:28 +0200

    Update hello-world.asm
    Added Assembly

--- hello-world.asm
+++ hello-world.asm
@@ -1,15 +1,18 @@
 section .data
-    hello db 'Hello World!', 0
+    message db 'Hello, World!', 0
 
 section .text
     global _start
 
 _start:
+    ; Write system call
     mov eax, 4
     mov ebx, 1
-    mov ecx, hello
+    mov ecx, message
     mov edx, 13
     int 0x80
+
+    ; Exit system call
     mov eax, 1
     xor ebx, ebx
     int 0x80
